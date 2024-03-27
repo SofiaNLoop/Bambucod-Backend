e95336c6013acf8dfa3ea65433b644119a915656
rsofianeira <rsofianeira@gmail.com>
2024-03-26 21:05:01 -0500

    add allowed url for allow frontend new url deployed in vercel

--- src/main/java/FinalProjectPeaku/Bambucod/config/WebConfig.java
+++ src/main/java/FinalProjectPeaku/Bambucod/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:3000")
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:3000", "https://bambucod-frontend.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
